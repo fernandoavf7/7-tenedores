ee6fabfe6fbaa19de87f05f0d282e67d0be3b726
ZNK <fernandoavf7@gmail.com>
2019-09-07 19:27:58 -0400

    final class ended proyect

--- app/views/Search.js
+++ app/views/Search.js
@@ -1,11 +1,93 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Text } from "react-native";
+import { SearchBar, ListItem, Icon } from 'react-native-elements';
+
+import { firebaseApp } from "./../utils/Firebase";
+import firebase from "firebase/app";
+import "firebase/firestore";
+const db = firebase.firestore(firebaseApp);
+import { FireSQL } from "firesql";
+const fireSQL = new FireSQL(firebase.firestore(), { includeId: 'id' });
+
 
 export default class Search extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            search: "",
+            restaurants: null
+        }
+    }
+
+    searchRestaurants = async (value) => {
+        this.setState({ search: value })
+        console.log(value)
+
+        let resultRestaurants = null;
+
+        const restaurants = fireSQL.query(
+            `SELECT * FROM restaurants 
+            WHERE name LIKE '${value}%'
+            `);
+
+        await restaurants.then(response => {
+            resultRestaurants = response;
+            console.log(resultRestaurants);
+
+        }).catch(error => {
+            //console.log(error);
+        })
+        this.setState({ restaurants: resultRestaurants })
+    }
+
+    clickRestaurant = (restaurant) => {
+        this.props.navigation.navigate("Restaurant", { restaurant })
+    }
+
+    renderListRestaurants = restaurants => {
+        if (restaurants) {
+            return (
+                <View>
+                    {restaurants.map((restaurant, index) => {
+                        let restaurantClick = {
+                            item: {
+                                restaurant: null
+                            }
+                        }
+                        restaurantClick.item.restaurant = restaurant;
+
+                        return (
+                            <ListItem
+                                onPress={() => this.clickRestaurant(restaurantClick)}
+                                key={index}
+                                title={restaurant.name}
+                                leftAvatar={{ source: { uri: restaurant.image } }}
+                                rightIcon={<Icon type="material-community"
+                                    name="chevron-right" />}
+                            />
+                        )
+                    })}
+                </View>
+            )
+        } else {
+            return (
+                <Text style={styles.notFoundText}>Busca tus restaurantes</Text>
+            )
+        }
+    }
+
     render() {
+        const { restaurants, search } = this.state;
         return (
-            <View style={styles.container}>
-                <Text>Search</Text>
+            <View style={styles.viewBody}>
+                <SearchBar
+                    placeholder="Buscar restaurantes..."
+                    onChangeText={this.searchRestaurants}
+                    value={search}
+                    containerStyle={styles.searchBar}
+                />
+                {this.renderListRestaurants(restaurants)}
             </View>
         )
     }
@@ -13,11 +95,13 @@ export default class Search extends Component {
 
 
 const styles = StyleSheet.create({
-    container: {
+    viewBody: {
         flex: 1,
-        backgroundColor: "#FFF",
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#FFF"
+    },
+    searchBar: {
+        marginBottom: 20
+    },
+    notFoundText: {
+        textAlign: "center"
     }
 })
